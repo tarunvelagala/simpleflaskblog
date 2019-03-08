0d533cf87c3686e73e129d14002c7d1d849fb61d
Tarun Kumar <tarunvelagala80@gmail.com>
2019-03-08 19:54:17 +0530

    Initial Commit

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
